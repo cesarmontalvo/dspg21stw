fe6c0e81de9f46df8c9e99b0cd8f589aa1b960da
Haleigh Tomlin <tomlinh22@mail.wlu.edu>
2021-06-28 09:09:53 -0400

    figuring out wioa probs

--- HT_wioa_datawork.R
+++ HT_wioa_datawork.R
@@ -4,26 +4,19 @@
 
 #install.packages("tidyverse")
 library(tidyverse)
+library(ggplot2)
+options(scipen = 999)
+
+
 
 #importing the data
 d <- read.csv("PY2019Q4.csv")
+head(d)
 
-#occupational codes
-length(unique(d$PIRL1610))
-unique(d$PIRL1610)
-
-#d[d$PIRL1610 == 536021,]) seems like a lot of NAs
-sum(is.na(d$PIRL1610) == FALSE)
-#okay, so there are 247,888 observations that have occupational codes.  out of 20 million observations!
 
 #using the wioa data dictionary 
-  #data in four quarters,
+#data in four quarters,
 
-dim(d) #21203183  308
-length(unique(d$PIRL100)) #12896358
-
-#with codes:
-d_codes <- d[is.na(d$PIRL610) == FALSE,]
 
 #PIRL100 = identifier
 #PIRL3000 = State ID (state that submitted the data, but WIOA hides the state the participant is from it seems)
@@ -64,14 +57,57 @@ d_codes <- d[is.na(d$PIRL610) == FALSE,]
 ## Occupational Certificate, Occupational Certification, other, none
 
 #PIRL3007 = WIOA measurable skill gains
-  ## 1 = skill gained, 0 = no skill gain
+## 1 = skill gained, 0 = no skill gain
 #PIRL3015 = WIOA median earnings (quarter 2)? -- maybe decide on what to use and from which quarters. 
-                # find out what the quarters are
+# find out what the quarters are
 
 #PIRL3023 - race/ethnicity single response (hispanic, asian, black, native hawaiian/pacific islander, american
-    #indian or alaska native, white, multiple race)
+#indian or alaska native, white, multiple race)
 
+d_smaller <- d %>% select(PIRL100, PIRL.3000, PIRL1610, PIRL1800, PIRL.3007, PIRL400, PIRL408, PIRL1300,
+                          PIRL1301, PIRL1303, PIRL1306, PIRL1310, PIRL1316,
+                          PIRL201, PIRL.3023)
 
-#d <- d %>% select() %>% mutate()
+d_smaller <- d_smaller %>% transmute(id = PIRL100,
+                                     state = PIRL.3000,
+                                     occup_code = PIRL1610,
+                                     type_cred = PIRL1800,
+                                     skill_gain_dummy = PIRL.3007,
+                                     emp_status = PIRL400,
+                                     edu_attain = PIRL408,
+                                     training_dummy = PIRL1300,
+                                     training_provider = PIRL1301,
+                                     training_type = PIRL1303,
+                                     skills_training1 = PIRL1306,
+                                     skills_training2 = PIRL1310,
+                                     skills_training3 = PIRL1316,
+                                     sex = PIRL201,
+                                     race = PIRL.3023)
 
-head(d)
+#seeing how the occupational codes line up
+
+View(d_smaller %>% select(occup_code, type_cred, training_dummy) %>% arrange(desc(occup_code)))
+# ^^ okay, so the occupational codes do line up to the onet codes, but some of them do not give me
+#search results while others do.
+
+#d[d$PIRL1610 == 536021,]) seems like a lot of NAs
+sum(is.na(d_smaller$occup_code) == FALSE)
+#okay, so there are 247,888 observations that have occupational codes.  out of 20 million observations!
+
+unique(d_smaller$training_type)
+
+sum(is.na(d_smaller$training_type) == FALSE)
+#there are 9 million observations that have values here!
+
+training_type_bar <- ggplot(d_smaller, aes(training_type)) + geom_bar()
+
+train <- d_smaller$training_type
+train <- sort(train)
+#names(train) <- c("none", "on the job", "skill upgrading", "entrepreneurial training", "ABE or ESL w other training",
+#                  "customized training", "occupational skills training", "ABE or ESL not in conjunction w training",
+#                  "prerequisite training", "registered apprenticeship", "youth occupational skills training",
+#                  "non-occupational skills training", "job readiness training in conjunction w other training")
+
+prop.table(table(train))
+#most people have 0 training, and then a bit more have customized training.
+
